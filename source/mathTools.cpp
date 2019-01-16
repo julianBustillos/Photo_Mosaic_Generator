f4c9a2aee3132331b6d52a1d7230b23bd7bee74b
Julian Bustillos <bustillosjulian@gmail.com>
2019-01-16 01:02:22 +0100

    Remove debug

--- source/mathTools.cpp
+++ source/mathTools.cpp
@@ -1,25 +1,8 @@
 #include "mathTools.h"
 #include <opencv2/opencv.hpp>
 
-//DEBUG
-#include <iostream>
-#include <assert.h>
 
-double cubicInterpolate(double p[4], double x) {
-    return p[1] + 0.5 * x*(p[2] - p[0] + x*(2.0*p[0] - 5.0*p[1] + 4.0*p[2] - p[3] + x*(3.0*(p[1] - p[2]) + p[3] - p[0])));
-}
-
-double bicubicInterpolate(double p[4][4], double x, double y) {
-    double arr[4];
-    arr[0] = cubicInterpolate(p[0], y);
-    arr[1] = cubicInterpolate(p[1], y);
-    arr[2] = cubicInterpolate(p[2], y);
-    arr[3] = cubicInterpolate(p[3], y);
-    return cubicInterpolate(arr, x);
-}
-//DEBUG
-
- const double MathTools::_biCubicCoeffs[16] = { -1, 3, -3, 1, 2, -5, 4, -1, -1, 0, 1, 0, 0, 2, 0, 0 };
+const double MathTools::_biCubicCoeffs[16] = { -1, 3, -3, 1, 2, -5, 4, -1, -1, 0, 1, 0, 0, 2, 0, 0 };
 
 
  int MathTools::clipInt(int val, int min, int max)
