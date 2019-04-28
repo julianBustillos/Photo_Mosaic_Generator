3de75b604da852d4c8296856b61afa760bfa5de5
Julian Bustillos <bustillosjulian@gmail.com>
2019-04-28 17:36:13 +0200

    Time accuracy print

--- include/clock.h
+++ include/clock.h
@@ -5,4 +5,4 @@
 
 
 #define TIME_NOW(time) std::chrono::high_resolution_clock::time_point time = std::chrono::high_resolution_clock::now();
-#define PRINT_DURATION(start, end) std::cout << "Execution time : " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " milliseconds" << std::endl << std::endl;
+#define PRINT_DURATION(start, end) int count = (int)std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count(); int min = count / 60000; int sec = (count - min * 60000) / 1000; int ms = count - min * 60000 - sec * 1000; std::cout << "Execution time : " << min << "min " << sec << "s " << ms << "ms" << std::endl << std::endl;
